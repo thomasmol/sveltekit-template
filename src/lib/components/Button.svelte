6222fb3175ac125e4fa259de53448d496f681711
Thomas Mol <email@thomasmol.com>
2023-02-05 16:15:32 +0100

    Added icons and click function to button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	export let type: 'button' | 'submit' | 'reset' | null | undefined = 'button';
-  export let styleClasses: string = '';
+	export let styleClasses: string = '';
+	export let onClick: () => void = () => {};
 </script>
 
 <button
 	{type}
+	on:click={onClick}
 	class="text-semibold rounded bg-neutral-800 px-4 py-2 text-white hover:bg-neutral-900 {styleClasses}">
 	<slot />
 </button>
